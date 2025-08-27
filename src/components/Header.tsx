56717947fe931a80f7923843e6875cf8ce500eda
theuxZn <matheusestevam421@gmail.com>
2025-08-26 21:16:19 -0300

    create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import { FaRegUser } from 'react-icons/fa';
+import { FiCpu } from 'react-icons/fi';
+import { RiMailSendLine } from 'react-icons/ri';
+import { GoProject } from 'react-icons/go';
+function Header() {
+	return (
+		<header className="w-full z-10 h-28 bg-transparent backdrop-blur-sm  fixed top-0 left-0 flex items-center justify-center p-6">
+			<div className="flex items-center gap-4 text-purple-bg">
+				<a href="$">
+					<div className="flex flex-col justify-center items-center gap-1">
+						<FaRegUser className="text-4xl" />
+						<p className="font-orbiton font-bold text-sm">Sobre</p>
+					</div>
+				</a>
+				<a href="$">
+					<div className="flex flex-col justify-center items-center gap-1">
+						<FiCpu className="text-4xl" />
+						<p className="font-orbiton font-bold text-sm">Tecnologias</p>
+					</div>
+				</a>
+				<a href="$">
+					<div className="flex flex-col justify-center items-center gap-1">
+						<GoProject className="text-4xl" />
+						<p className="font-orbiton font-bold text-sm">Projetos</p>
+					</div>
+				</a>
+				<a href="$">
+					<div className="flex flex-col justify-center items-center gap-1">
+						<RiMailSendLine className="text-4xl" />
+						<p className="font-orbiton font-bold text-sm">Contato</p>
+					</div>
+				</a>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
