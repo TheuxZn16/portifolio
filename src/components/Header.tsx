ccdd8b35395061315cfcd230ee12f6ca0c7149e9
theuxZn <matheusestevam421@gmail.com>
2025-08-31 00:22:52 -0300

    modify header pt1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,23 @@ import { RiMailSendLine } from 'react-icons/ri';
 import { GoProject } from 'react-icons/go';
 function Header() {
 	return (
-		<header className="w-screen z-10 h-28 bg-transparent backdrop-blur-sm  fixed top-0 left-0 flex items-center justify-center p-6">
-			<div className="flex items-center gap-4 bg-gradient-to-br from-purple-primary to-cyan-primary text-transparent bg-clip-text">
+		<header
+			className="w-4/5 z-10 h-28 bg-zinc-600 backdrop-blur-sm fixed top-12 left-[50%] rounded-sm transform-[translate(-50%)] flex items-center justify-between py-6 lg:px-28 md:px-16 px-8"
+			style={{
+				clipPath:
+					'polygon(30px 0, 100% 0, 100% calc(100% - 30px), calc(100% - 30px) 100%, 57.5% 100%, 54% calc(100% - 10px), 46% calc(100% - 10px), 42.5% 100%, 0 100%, 0 30px)',
+			}}
+		>
+			<div className="flex justify-center items-center">
+				<a href="$">
+					<img
+						src="/images/Logo1.png"
+						alt="Logo TheusDev"
+						className="size-32"
+					/>
+				</a>
+			</div>
+			<div className="items-center hidden md:flex gap-2 min-[860px]:gap-4 bg-gradient-to-br from-purple-primary to-cyan-primary text-transparent bg-clip-text">
 				<a href="$">
 					<div className="flex flex-col justify-center items-center gap-1">
 						<FaRegUser className="text-4xl text-cyan-primary" />
