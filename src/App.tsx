99571ee02b3fd94698b8a387485ce313aca33a11
theuxZn <matheusestevam421@gmail.com>
2025-09-12 19:01:42 -0300

    set redisign portifolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import Header from './components/Header';
-import LogoScroll from './components/LogoMain';
-
 function App() {
 	return (
-		<div className="bg-[#0a0a0a]">
-			<Header />
-			<div className="h-[100vh]"></div>
-			<LogoScroll url="/images/Logo2.svg" />
-
-			<div className="h-screen"></div>
-			<div className="h-screen"></div>
-			<div className="h-screen"></div>
-			<div className="h-screen"></div>
-			<div className="h-screen"></div>
-			<div className="h-screen"></div>
-			<div className="h-screen"></div>
+		<div className="bg-[#0a0a0a] flex justify-center items-center h-screen ">
+			<h1 className="text-8xl text-amber-600">Hello world!</h1>
 		</div>
 	);
 }
