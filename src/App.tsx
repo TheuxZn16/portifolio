1785016fb8161ad4a4c8cbd7eddb4bd1cf72012a
theuxZn <matheusestevam421@gmail.com>
2025-10-10 18:49:44 -0300

    Created first Hero page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
 	return (
 		<div className="bg-[#0a0a0a] ">
 			<Header />
+			<Hero />
 			<div className="h-screen "></div>
 			<div className="h-screen "></div>
 			<div className="h-screen"></div>
